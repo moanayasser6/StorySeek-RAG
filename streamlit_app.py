edcb57415b0cd65d6fb9e7da8e6b4c214fd397c2
moanayasser6 <moanayasser6@gmail.com>
2026-07-26 20:10:02 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import faiss
+import joblib
+import torch
 
-# --------------------------------------------------
-# Page Configuration
-# --------------------------------------------------
+from sentence_transformers import SentenceTransformer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 st.set_page_config(
     page_title="StorySeek",
@@ -10,48 +14,9 @@ st.set_page_config(
     layout="wide"
 )
 
-# --------------------------------------------------
-# Header
-# --------------------------------------------------
+ARTIFACTS_PATH = "artifacts"
 
-st.title("📚 StorySeek")
-st.subheader("Searching Ancient Stories with Modern AI")
-
-st.markdown(
-    """
-Search Arabic stories using a Hybrid Retrieval System
-combining BM25, FAISS, and Large Language Models.
-"""
-)
-
-st.divider()
-
-# --------------------------------------------------
-# Search Area
-# --------------------------------------------------
-
-query = st.text_input(
-    "Enter your question:",
-    placeholder="Example: قصة عن الشجاعة"
-)
-
-search_button = st.button("Search")
-
-st.divider()
-
-# --------------------------------------------------
-# Results Area
-# --------------------------------------------------
-
-if search_button:
-
-    if query.strip() == "":
-        st.warning("Please enter a question.")
-
-    else:
-        st.success("Search button works successfully!")
-
-        st.write("Your query:")
-
-        st.code(query)
-           
+CHUNKS_PATH = f"{ARTIFACTS_PATH}/chunks.csv"
+EMBEDDINGS_PATH = f"{ARTIFACTS_PATH}/embeddings.npy"
+FAISS_PATH = f"{ARTIFACTS_PATH}/faiss_index.index"
+BM25_PATH = f"{ARTIFACTS_PATH}/bm25.pkl"
